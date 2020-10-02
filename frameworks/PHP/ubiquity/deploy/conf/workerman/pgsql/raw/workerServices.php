161c9a7719d18e450c176267e5971567b77874a7
jcheron <myaddressmail@gmail.com>
2020-10-02 18:29:58 +0200

    add raw files

---
+++ frameworks/PHP/ubiquity/deploy/conf/workerman/pgsql/raw/workerServices.php
@@ -0,0 +1,14 @@
+<?php
+\Ubiquity\cache\CacheManager::startProd($config);
+
+\Ubiquity\cache\CacheManager::warmUpControllers([
+	\controllers\DbRaw::class,
+	\controllers\FortunesRaw::class
+]);
+
+$workerServer->onWorkerStart = function () use ($config) {
+	$db = \Ubiquity\db\Database::start('pgsql', $config);
+	\controllers\DbRaw::warmup($db);
+	\controllers\FortunesRaw::warmup($db);
+};
+
