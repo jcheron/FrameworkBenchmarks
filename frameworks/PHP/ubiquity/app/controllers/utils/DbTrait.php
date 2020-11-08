aa92051da3a78bf561beccf749b34b206d96748d
jcheron <myaddressmail@gmail.com>
2020-11-08 14:02:59 +0100

    update getCount

--- frameworks/PHP/ubiquity/app/controllers/utils/DbTrait.php
+++ frameworks/PHP/ubiquity/app/controllers/utils/DbTrait.php
@@ -3,13 +3,18 @@ namespace controllers\utils;
 
 trait DbTrait {
 
+	private $counts = [];
+
 	public function getCount($queries) {
+		if ($this->counts[$queries]) {
+			return $this->counts[$queries];
+		}
 		$count = 1;
 		if ($queries > 1) {
 			if (($count = $queries) > 500) {
 				$count = 500;
 			}
 		}
-		return $count;
+		return $this->counts[$queries] = $count;
 	}
 }
