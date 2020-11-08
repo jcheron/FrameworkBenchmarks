05004401090f1d5667b7e379012fe253fe07d754
jcheron <myaddressmail@gmail.com>
2020-11-08 14:39:36 +0100

    update getCount

--- frameworks/PHP/ubiquity/app/controllers/utils/DbTrait.php
+++ frameworks/PHP/ubiquity/app/controllers/utils/DbTrait.php
@@ -6,15 +6,6 @@ trait DbTrait {
 	private $counts = [];
 
 	public function getCount($queries) {
-		if (isset($this->counts[$queries])) {
-			return $this->counts[$queries];
-		}
-		$count = 1;
-		if ($queries > 1) {
-			if (($count = $queries) > 500) {
-				$count = 500;
-			}
-		}
-		return $this->counts[$queries] = $count;
+		return $this->counts[$queries] ??= \min(\max($queries, 1), 500);
 	}
 }
